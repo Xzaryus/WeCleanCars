49e01cca7228071d449bff74551e7acaa10b925c
Xzaryus <nicocodes@hotmail.com>
2026-04-08 11:50:32 +0100

    Added :Manager dashboard

--- Frontend/WeCleanCars/src/pages/admin/ManagerDashboard.jsx
+++ Frontend/WeCleanCars/src/pages/admin/ManagerDashboard.jsx
@@ -1,3 +1,37 @@
-export default function AdminLogin() {
-    return <div>Manager Dashboard</div>;
+import { useState } from "react";
+import { useOutletContext, useNavigate } from "react-router-dom";
+import { views } from "./ViewsConfig";
+import { ManagerCards } from "../../components/DashboardCards";
+export default function ManagerDashboard() {
+    const [ , setUser] = useOutletContext();
+    const navigate = useNavigate();
+    const [view, setView] = useState(null);
+    const handleLogout = () => {
+        setUser(null);
+        navigate("/admin");
+    }
+
+    const active = views.find(v => v.key === view);
+
+    //render the active view component
+    if (active) {
+        const ActiveComponent = active.component; // must be capitalized
+        return <ActiveComponent onBack={() => setView(null)} />;
+    }
+
+    return (
+        <>  
+            <button className="logoutButton" onClick={handleLogout}>Logout</button>
+            <div className="dashboardCards">
+                {views.map(view => (
+                    <ManagerCards className="dashboardCard"
+                    icon={view.icon}
+                    key={view.key}
+                    title={view.title}
+                    onClick={() => setView(view.key)}
+                    /> 
+                ))}
+            </div>
+        </>
+    );
 }
